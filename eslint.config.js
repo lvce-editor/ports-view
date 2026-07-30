53ec9bf30b86c55f0e5122e9e2f4e903fc763f18
Le Vivilet <levivilet@pm.me>
2026-07-30 09:48:17 +0000

    feature: add knip

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import * as config from '@lvce-editor/eslint-config'
-import * as actions from '@lvce-editor/eslint-plugin-github-actions/dist/index.js'
+import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config.default,
--- packages/ports-view/src/parts/PortsStates/PortsStates.ts
+++ packages/ports-view/src/parts/PortsStates/PortsStates.ts
@@ -1,4 +1,4 @@
 import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
 import type { PortsState } from '../PortsState/PortsState.ts'
 
-export const { diff, dispose, get, getCommandIds, registerCommands, set, wrapCommand, wrapGetter } = ViewletRegistry.create<PortsState>()
+export const { diff, dispose, get, getCommandIds, registerCommands, set, wrapCommand } = ViewletRegistry.create<PortsState>()
