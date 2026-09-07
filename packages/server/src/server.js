6f3e2ecea682c87ee79269d2088ee831ae9aa114
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2026-09-07 14:17:42 +0000

    fix: link published test worker into ports test server

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -1,6 +1,11 @@
 import { serverPath } from '@lvce-editor/server'
 import { fileURLToPath, pathToFileURL } from 'node:url'
 
-process.argv.push('--link', fileURLToPath(new URL('../../../.tmp/dist', import.meta.url)))
+process.argv.push(
+  '--link',
+  fileURLToPath(new URL('../../../.tmp/dist', import.meta.url)),
+  '--link',
+  fileURLToPath(new URL('../../../node_modules/@lvce-editor/test-worker', import.meta.url)),
+)
 
 await import(pathToFileURL(serverPath).href)
