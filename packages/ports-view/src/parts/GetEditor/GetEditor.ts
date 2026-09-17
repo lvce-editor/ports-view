61e7aa8ba23418b536d27ae344a0d55c45f3c0b6
Le Vivilet <levivilet@pm.me>
2026-09-17 09:07:36 -0700

    feature: simplify port input dom (#36)

--- packages/ports-view/src/parts/GetEditor/GetEditor.ts
+++ packages/ports-view/src/parts/GetEditor/GetEditor.ts
@@ -12,14 +12,8 @@ const cancelButton: VirtualDomNode = {
   type: VirtualDomElements.Button,
 }
 
-export const getEditor = (state: PortsState): readonly VirtualDomNode[] => {
-  const { addPortError, addPortValue } = state
+const getPortInputDom = (addPortValue: string): readonly VirtualDomNode[] => {
   return [
-    {
-      childCount: addPortError ? 4 : 3,
-      className: ClassNames.AddPortEditor,
-      type: VirtualDomElements.Div,
-    },
     {
       ariaLabel: PortsStrings.portNumber(),
       childCount: 0,
@@ -31,6 +25,18 @@ export const getEditor = (state: PortsState): readonly VirtualDomNode[] => {
       type: VirtualDomElements.Input,
       value: addPortValue,
     },
+  ]
+}
+
+export const getEditor = (state: PortsState): readonly VirtualDomNode[] => {
+  const { addPortError, addPortValue } = state
+  return [
+    {
+      childCount: addPortError ? 4 : 3,
+      className: ClassNames.AddPortEditor,
+      type: VirtualDomElements.Div,
+    },
+    ...getPortInputDom(addPortValue),
     {
       childCount: 1,
       className: ClassNames.AddPortButton,
