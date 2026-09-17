67be8014472bde9ec126887c3b519dfb53b5c739
Le Vivilet <levivilet@pm.me>
2026-09-17 06:18:48 -0700

    feature: enable accessibility e2e test (#33)

--- packages/e2e/src/ports-view.accessibility.ts
+++ packages/e2e/src/ports-view.accessibility.ts
@@ -1,6 +1,6 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const skip = 1
+// export const skip = 1
 
 export const test: Test = async ({ expect, Ports }) => {
   await Ports.open()
