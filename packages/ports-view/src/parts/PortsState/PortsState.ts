c40fd222f66d4c6e840997cbeb1ee20ccbbff883
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-24 13:09:19 +0000

    fix: sort page scroll multiplier state fields

--- packages/ports-view/src/parts/PortsState/PortsState.ts
+++ packages/ports-view/src/parts/PortsState/PortsState.ts
@@ -18,8 +18,8 @@ export interface PortsState {
   readonly maxLineY: number
   readonly minimumScrollBarSize: number
   readonly minLineY: number
-  readonly parentUid?: number
   readonly pageScrollMultiplier: number
+  readonly parentUid?: number
   readonly platform: number
   readonly ports: readonly Port[]
   readonly scrollBarHeight: number
